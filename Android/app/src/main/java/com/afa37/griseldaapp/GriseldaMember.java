47f36587470083dd7bfce183a9561f1853b8194f
AfeefahA <59742669+AfeefahA@users.noreply.github.com>
2020-02-06 18:44:10 -0500

    First check-in
    Griselda, real rap for real rap listeners.

---
+++ Android/app/src/main/java/com/afa37/griseldaapp/GriseldaMember.java
@@ -0,0 +1,9 @@
+package com.afa37.griseldaapp;
+
+public class GriseldaMember extends Rapper {
+
+    public GriseldaMember() {
+        haveBars = true;
+        canSing = false;
+    }
+}
---
+++ Android/app/src/main/java/com/afa37/griseldaapp/Rapper.java
@@ -0,0 +1,7 @@
+package com.afa37.griseldaapp;
+
+public class Rapper {
+
+    public boolean haveBars;
+    public boolean canSing;
+}
